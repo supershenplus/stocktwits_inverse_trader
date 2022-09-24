c6f56e72f6b803276eb6eee3a5108dd8bd2224c0
gavin jordan <gtj1997@gmail.com>
2022-09-23 20:10:29 -0700

    used py blank linter to update formatting and fixed bug with order logic, as well as adding a short selling feature

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import time
 # selenium is the web automation tool we use to navigate through and find the data we need on webpages
 chromedriver_autoinstaller.install()
 op = webdriver.ChromeOptions()
-op.add_argument('--headless')
-op.add_argument('--log-level=3')
+op.add_argument("--headless")
+op.add_argument("--log-level=3")
 page = webdriver.Chrome(options=op)
 # alpaca markets API is used to place orders linked to our paper trading account
 trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
@@ -22,23 +22,24 @@ account = trading_client.get_account()
 
 random_stonk = []
 
-
 # randomly generate a stock using a different project and then checks the stock to see if it is available on stocktwits
 # if the stock is not available then it will loop until an availble stock is found
 def randomize_stonk():
-    page.get('https://raybb.github.io/random-stock-picker/')
-    gen_stonk = page.find_element(By.ID, 'refresh')
+    page.get("https://raybb.github.io/random-stock-picker/")
+    gen_stonk = page.find_element(By.ID, "refresh")
     gen_stonk.click()
-    find_stonk = page.find_element(By.ID, 'ticker')
+    find_stonk = page.find_element(By.ID, "ticker")
     random_stonk.clear()
     random_stonk.append(find_stonk.text)
     print("Stock found: ", random_stonk[0])
-    page.get('https://stocktwits.com/symbol/' + random_stonk[0])
+    page.get("https://stocktwits.com/symbol/" + random_stonk[0])
     time.sleep(1)
 
     # checking to see if stocktwits returns an "Uh-oh, we can't find this stock" page
     # there is probably a cleaner way to check if the ticker is not available on stocktwits
-    stock_real = re.search('Uh-oh, we (.*)an', page.find_element(By.XPATH, "/html/body").text)
+    stock_real = re.search(
+        "Uh-oh, we (.*)an", page.find_element(By.XPATH, "/html/body").text
+    )
     if stock_real is None:
         print("Analyzing StockTwits sentiment...")
     else:
@@ -51,42 +52,43 @@ randomize_stonk()
 # scrolls down through the feed to collect data, includes a time delay to help buffer the action without causing issues
 for _ in range(50):
     webdriver.ActionChains(page).key_down(Keys.PAGE_DOWN).perform()
-    time.sleep(.6)
-
+    time.sleep(0.6)
 # aggregating bearish and bullish tags affixed to posts
-bear = 'Bearish'
-bull = 'Bullish'
+bear = "Bearish"
+bull = "Bullish"
 get_bear = len(re.findall(bear, page.find_element(By.XPATH, "/html/body").text))
 get_bull = len(re.findall(bull, page.find_element(By.XPATH, "/html/body").text))
-print('Sentiment analysis complete.')
-print('Bearish count: ', get_bear)
-print('Bullish count: ', get_bull)
+print("Sentiment analysis complete.")
+print("Bearish count: ", get_bear)
+print("Bullish count: ", get_bull)
 
+# end selenium session
 page.quit()
 
 # defining market order parameters
-market_order_data = MarketOrderRequest(
-    symbol=random_stonk[0],
-    qty=1,
-    side=OrderSide.BUY,
-    time_in_force=TimeInForce.GTC
+market_buy = MarketOrderRequest(
+    symbol=random_stonk[0], qty=1, side=OrderSide.BUY, time_in_force=TimeInForce.GTC
 )
 
-# this is triggering an order
-market_order = trading_client.submit_order(order_data=market_order_data)
+market_sell = MarketOrderRequest(
+    symbol=random_stonk[0], qty=1, side=OrderSide.SELL, time_in_force=TimeInForce.GTC
+)
+
 
 # order logic
-# todo: get this to work :^).. for some reason this is not even triggering any of the print statements it
-#  triggered the first if statement once, but not the others, confirmed last elif is not working
 if get_bear > get_bull:
-    print('Inversing StockTwits sentiment....')
-    print('Order for ' + random_stonk[0] + ' placed.')
-elif get_bear < get_bull and trading_client.get_asset(random_stonk[0]).shortable is True:
-    print('Inversing StockTwits sentiment....')
-    print('Short order for ' + random_stonk[0] + ' placed.')
-elif get_bear < get_bull and trading_client.get_asset(random_stonk[0]).shortable is False:
-    print('Cannot short this stock on Alpaca. Please choose another stock.')
+    print("Inversing StockTwits sentiment....")
+    trading_client.submit_order(order_data=market_buy)
+    print("Order for " + random_stonk[0] + " placed.")
+elif (
+    get_bear < get_bull and trading_client.get_asset(random_stonk[0]).shortable is True
+):
+    print("Inversing StockTwits sentiment....")
+    trading_client.submit_order(order_data=market_sell)
+    print("Short order for " + random_stonk[0] + " placed.")
+elif (
+    get_bear < get_bull and trading_client.get_asset(random_stonk[0]).shortable is False
+):
+    print("Cannot short this stock on Alpaca. Please choose another stock.")
 elif get_bear and get_bull == 0:
-    print('Not enough sentiment data to place order.'
-          '\n Please try again')
-
+    print("Not enough sentiment data to place order." "\n Please try again")
