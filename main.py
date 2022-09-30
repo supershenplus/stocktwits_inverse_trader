6713d8a06aaea0007334aa755427d16760243b8e
gavin jordan <gtj1997@gmail.com>
2022-09-29 17:20:18 -0700

    updated a few comments

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ op.add_argument("--log-level=3")
 page = webdriver.Chrome(options=op)
 # alpaca markets API is used to place orders linked to our paper trading account
 trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
-account = trading_client.get_account()
 
-random_stonk = []
 
-# randomly generate a stock using a different project and then checks the stock to see if it is available on stocktwits
-# if the stock is not available then it will loop until an availble stock is found
+random_stonk = 'undefined'
+
+
+# grab a random stock and stores it in random_stonk
 def randomize_stonk():
     page.get("https://raybb.github.io/random-stock-picker/")
     gen_stonk = page.find_element(By.ID, "refresh")
     gen_stonk.click()
     find_stonk = page.find_element(By.ID, "ticker")
-    random_stonk.clear()
-    random_stonk.append(find_stonk.text)
-    print("Stock found: ", random_stonk[0])
-    page.get("https://stocktwits.com/symbol/" + random_stonk[0])
+    global random_stonk
+    random_stonk = find_stonk.text
+    print("Stock found: ", random_stonk)
+    page.get("https://stocktwits.com/symbol/" + random_stonk)
     time.sleep(1)
 
     # checking to see if stocktwits returns an "Uh-oh, we can't find this stock" page
@@ -49,7 +49,8 @@ def randomize_stonk():
 
 randomize_stonk()
 
-# scrolls down through the feed to collect data, includes a time delay to help buffer the action without causing issues
+# scrolls down through the stocktwits feed to collect data, includes a time delay to help buffer the action without
+# causing issues
 for _ in range(50):
     webdriver.ActionChains(page).key_down(Keys.PAGE_DOWN).perform()
     time.sleep(0.6)
@@ -65,29 +66,28 @@ print("Bullish count: ", get_bull)
 # end selenium session
 page.quit()
 
-# defining market order parameters
+# defining buy and sell market order parameters
 market_buy = MarketOrderRequest(
-    symbol=random_stonk[0], qty=1, side=OrderSide.BUY, time_in_force=TimeInForce.GTC
+    symbol=random_stonk, qty=1, side=OrderSide.BUY, time_in_force=TimeInForce.GTC
 )
 
 market_sell = MarketOrderRequest(
-    symbol=random_stonk[0], qty=1, side=OrderSide.SELL, time_in_force=TimeInForce.GTC
+    symbol=random_stonk, qty=1, side=OrderSide.SELL, time_in_force=TimeInForce.GTC
 )
 
-
-# order logic
+# order logic which buys if the overall sentiment is bearish and sells if the overall sentiment is bullish
 if get_bear > get_bull:
     print("Inversing StockTwits sentiment....")
     trading_client.submit_order(order_data=market_buy)
-    print("Order for " + random_stonk[0] + " placed.")
+    print("Long order for " + random_stonk + " placed.")
 elif (
-    get_bear < get_bull and trading_client.get_asset(random_stonk[0]).shortable is True
+    get_bear < get_bull and trading_client.get_asset(random_stonk).shortable is True
 ):
     print("Inversing StockTwits sentiment....")
     trading_client.submit_order(order_data=market_sell)
-    print("Short order for " + random_stonk[0] + " placed.")
+    print("Short order for " + random_stonk + " placed.")
 elif (
-    get_bear < get_bull and trading_client.get_asset(random_stonk[0]).shortable is False
+    get_bear < get_bull and trading_client.get_asset(random_stonk).shortable is False
 ):
     print("Cannot short this stock on Alpaca. Please choose another stock.")
 elif get_bear and get_bull == 0:
